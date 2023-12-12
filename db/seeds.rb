3e3b1d2cab30c92ddc9d59f4889a6f1099b47261
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-12 22:18:39 +1100

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Airports
+a1 = Airport.find_or_create_by!(name: "Belgrad (Beograd) - Belgrade Nikola Tesla International, Serbia", code: "BEG")
+a2 = Airport.find_or_create_by!(name: "London - Gatwick, United Kingdom", code: "LGW")
+a3 = Airport.find_or_create_by!(name: "Los Angeles (CA) - International, USA", code: "LAX")
+a4 = Airport.find_or_create_by!(name: "Shanghai - Pu Dong, China", code: "PVG")
+a5 = Airport.find_or_create_by!(name: "Sydney - Sydney Airport, Australia", code: "SYD")
+
+# Flights
+f1 = Flight.find_or_create_by!(departure_airport: a1, arrival_airport: a2, start: DateTime.new(2024,1,1,6), duration: 60)
+f2 = Flight.find_or_create_by!(departure_airport: a2, arrival_airport: a3, start: DateTime.new(2024,1,2,6), duration: 60)
+f3 = Flight.find_or_create_by!(departure_airport: a3, arrival_airport: a4, start: DateTime.new(2024,1,3,6), duration: 60)
+f4 = Flight.find_or_create_by!(departure_airport: a4, arrival_airport: a5, start: DateTime.new(2024,1,4,6), duration: 60)
+f5 = Flight.find_or_create_by!(departure_airport: a5, arrival_airport: a1, start: DateTime.new(2024,1,5,6), duration: 60)
