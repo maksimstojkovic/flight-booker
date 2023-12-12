204d65769555e417b801856e1208f68bec6575f0
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-12 23:36:53 +1100

    Add flight search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,6 @@
 class FlightsController < ApplicationController
   def index
+    @airport_options = Airport.all.map { |airport| [airport.name, airport.id] }
+    @date_options = Flight.all.map { |flight| [flight.date.to_date, flight.date.to_date] }
   end
 end
