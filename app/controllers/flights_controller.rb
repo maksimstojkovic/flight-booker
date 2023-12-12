07b31ba5131aaeab5f7311b02c76a998a8498f32
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-12 23:45:41 +1100

    Update date format

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,6 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |airport| [airport.name, airport.id] }
-    @date_options = Flight.all.map { |flight| [flight.date.to_date, flight.date.to_date] }
+    @date_options = Flight.all.map { |flight| [flight.date.to_date.strftime("%d/%m/%Y"), flight.date.to_date] }
   end
 end
