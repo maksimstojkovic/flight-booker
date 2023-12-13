e494e3cf8bb65bc8e08cdf5b6d93e8a5537cbc57
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-13 20:56:17 +1100

    Add flight search results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,19 @@ class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |airport| [airport.name, airport.id] }
     @date_options = Flight.all.map { |flight| [flight.date.to_date.strftime("%d/%m/%Y"), flight.date.to_date] }
+    @search_params = search_params
+
+    if search_params.to_hash.map{ |k, v| v != "" }.any?
+      @results = Flight.all
+      @results = @results.where(departure_airport_id: search_params[:departure_airport]) if search_params[:departure_airport] != ""
+      @results = @results.where(arrival_airport_id: search_params[:arrival_airport]) if search_params[:arrival_airport] != ""
+      @results = @results.where(date: search_params[:date]) if search_params[:date] != ""
+    end
+  end
+
+  private
+
+  def search_params
+    p = params.permit(:departure_airport, :arrival_airport, :passengers, :date)
   end
 end
