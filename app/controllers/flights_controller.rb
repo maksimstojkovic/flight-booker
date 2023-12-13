7b0c4f127998cb4c75006c79ecd1f76e717b89a9
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-13 21:30:23 +1100

    Update search to use codes

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,13 +1,13 @@
 class FlightsController < ApplicationController
   def index
-    @airport_options = Airport.all.map { |airport| [airport.name, airport.id] }
+    @airport_options = Airport.all.map { |airport| [airport.name, airport.code] }
     @date_options = Flight.all.map { |flight| [flight.date.to_date.strftime("%d/%m/%Y"), flight.date.to_date] }
     @search_params = search_params
 
     if search_params.to_hash.map{ |k, v| v != "" }.any?
-      @results = Flight.all
-      @results = @results.where(departure_airport_id: search_params[:departure_airport]) if search_params[:departure_airport] != ""
-      @results = @results.where(arrival_airport_id: search_params[:arrival_airport]) if search_params[:arrival_airport] != ""
+      @results = Flight.joins(:departure_airport, :arrival_airport)
+      @results = @results.where(departure_airport: { code: search_params[:departure_airport] }) if search_params[:departure_airport] != ""
+      @results = @results.where(arrival_airport: { code: search_params[:arrival_airport] }) if search_params[:arrival_airport] != ""
       @results = @results.where(date: search_params[:date]) if search_params[:date] != ""
     end
   end
