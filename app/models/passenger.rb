351911b109c8cd5311c339bd24b656029a62920d
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-16 03:35:03 +1100

    Add passenger validation

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,7 @@
 class Passenger < ApplicationRecord
   has_many :bookings
   has_many :flights, through: :bookings
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :email, presence: true, length: { minimum: 3, maximum: 100 }, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
