fe8382821991939ff530918e4970e9f0271fce1e
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-15 22:31:40 +1100

    Add booking form input parameter validation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,24 @@
 class BookingsController < ApplicationController
   def new
+    validate_booking_params
   end
 
   def create
+  end
+
+  private
+
+  def validate_booking_params
+    if %i[ flight passengers ].map { |s| !params.has_key?(s) || params[s].empty? }.any?
+      if request.referer.nil?
+        redirect_to root_url
+      else
+        redirect_to request.referer, status: :unprocessable_entity
+      end
+    end
+  end
+
+  def booking_params
+    params.permit(:flight, :passengers)
   end
 end
